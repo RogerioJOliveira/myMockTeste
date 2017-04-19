b599b58d88626dfdaf51748e92840a85a39f0675
RogerioJOliveira <RogerioJOliveira@gmail.com>
2017-04-19 15:32:02 -0300

    mock final

--- app/server/boot/update_sqlite_schema.js
+++
@@ -1,7 +0,0 @@
- module.exports = function(app) {
-    app.dataSources.sqlite3.isActual(function(err, actual){
-      if (!actual) {
-        app.dataSources.sqlite3.autoupdate();
-      }
-    });
-  };
